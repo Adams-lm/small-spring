339064f6feebfbff1038a8e5247141663769ea72
Adams-lm <865725198@qq.com>
2023-11-30 15:53:01 +0800

    remove 05

--- small-spring-step-05/src/main/java/cn/bugstack/springframework/beans/factory/support/DefaultListableBeanFactory.java
+++ small-spring-step-05/src/main/java/cn/bugstack/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -12,7 +12,7 @@ import java.util.Map;
 /**
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
-public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements BeanDefinitionRegistry, ConfigurableListableBeanFactory {
+public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory {
 
     private Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
@@ -21,29 +21,7 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
         beanDefinitionMap.put(beanName, beanDefinition);
     }
 
-    @Override
-    public boolean containsBeanDefinition(String beanName) {
-        return beanDefinitionMap.containsKey(beanName);
-    }
 
-    @Override
-    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
-        Map<String, T> result = new HashMap<>();
-        beanDefinitionMap.forEach((beanName, beanDefinition) -> {
-            Class beanClass = beanDefinition.getBeanClass();
-            if (type.isAssignableFrom(beanClass)) {
-                result.put(beanName, (T) getBean(beanName));
-            }
-        });
-        return result;
-    }
-
-    @Override
-    public String[] getBeanDefinitionNames() {
-        return beanDefinitionMap.keySet().toArray(new String[0]);
-    }
-
-    @Override
     public BeanDefinition getBeanDefinition(String beanName) throws BeansException {
         BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
         if (beanDefinition == null) throw new BeansException("No bean named '" + beanName + "' is defined");
