11d75fe0ad2e4a7591dd6dbe36fef5313f14f42b
Adams-lm <865725198@qq.com>
2023-12-05 15:19:22 +0800

    commit 05

--- small-spring-step-05/src/main/java/cn/bugstack/springframework/beans/factory/support/DefaultListableBeanFactory.java
+++ small-spring-step-05/src/main/java/cn/bugstack/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -3,16 +3,14 @@ package cn.bugstack.springframework.beans.factory.support;
 import cn.bugstack.springframework.beans.BeansException;
 import cn.bugstack.springframework.beans.factory.ConfigurableListableBeanFactory;
 import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
-import cn.bugstack.springframework.beans.factory.config.ConfigurableBeanFactory;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
-public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory {
+public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory  implements BeanDefinitionRegistry, ConfigurableListableBeanFactory {
 
     private Map<String, BeanDefinition> beanDefinitionMap = new HashMap<>();
 
@@ -27,5 +25,29 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
         if (beanDefinition == null) throw new BeansException("No bean named '" + beanName + "' is defined");
         return beanDefinition;
     }
+
+    @Override
+    public boolean containsBeanDefinition(String beanName) {
+        return beanDefinitionMap.containsKey(beanName);
+    }
+
+    @Override
+    public String[] getBeanDefinitionNames() {
+        return beanDefinitionMap.keySet().toArray(new String[0]);
+    }
+
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        Map<String, T> result = new HashMap<>();
+        beanDefinitionMap.forEach((beanName, beanDefinition) -> {
+            Class beanClass = beanDefinition.getBeanClass();
+            if (type.isAssignableFrom(beanClass)) {
+                result.put(beanName, (T) getBean(beanName));
+            }
+        });
+        return result;
+    }
+
+
 
 }
